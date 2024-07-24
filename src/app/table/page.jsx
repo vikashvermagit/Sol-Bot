ba5f9bdd48227cb31906525f46d9d29acf7b684a
umeshkh123 <umeshkhantwal@nexocide.com>
2024-07-23 22:17:48 -0700

    table

---
+++ src/app/table/page.jsx
@@ -0,0 +1,14 @@
+import PendingPoolTable from "@/components/PendingPoolTable";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <div>
+        <PendingPoolTable />
+      </div>
+    </>
+  );
+};
+
+export default Page;
